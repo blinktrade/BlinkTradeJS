e5882074777bba66874ea8d2d6dd850536d41b83
cesardeazevedo <cesardeazevedo@outlook.com>
2016-09-02 00:10:56 -0300

    Implemented brokers data

---
+++ src/constants/brokers.js
@@ -0,0 +1,8 @@
+export default {
+  SURBITCOIN: 1,
+  VBTC: 3,
+  FOXBIT: 4,
+  TESTNET: 5,
+  URDUBIT: 8,
+  CHILEBIT: 9,
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import Brokers from './constants/brokers';
 import BlinkTradeWS from './blinktradeWS';
 import BlinkTradeRest from './blinktradeRest';
 
 export {
+  Brokers,
   BlinkTradeWS,
   BlinkTradeRest,
 };
