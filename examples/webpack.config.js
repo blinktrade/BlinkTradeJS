50770c194fafd4eaef19431b24be2abff31c761e
Cesar Augusto <cesardeazevedo@outlook.com>
2017-04-09 01:28:00 -0300

    Updated webpack example
    Alias blinktrade to blinktrade/browser

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,39 +1,36 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
+  target: 'web',
+  resolve: {
+    alias: {
+      blinktrade: 'blinktrade/browser',
+    },
+  },
   entry: {
     authWebSocket: './authWebSocket',
     balance: './balance',
     deposits: './deposits',
+    myOrders: './myOrders',
     eventEmitters: './eventEmitters',
     executionReport: './executionReport',
-    listWithdraws: './listWithdraws',
     orderBookWebSocket: './orderBookWebSocket',
     tickerWebSocket: './tickerWebSocket',
     tradeHistory: './tradeHistory',
     sendAndCancelOrdersRest: './sendAndCancelOrdersRest',
     sendAndCancelOrdersWebSocket: './sendAndCancelOrdersWebSocket',
-    withdraw: './withdraw'
+    withdraw: './withdraw',
   },
   output: {
     path: path.join(__dirname, 'bundle'),
-    filename: '[name].bundle.js'
-  },
-  node: {
-    ws: 'empty',
-    fs: 'empty',
-    tls: 'empty',
-    dgram: 'empty',
-    encoding: 'empty',
-    child_process: 'empty'
+    filename: '[name].bundle.js',
   },
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
-    }]
-  }
-}
+      loader: 'babel-loader',
+    }],
+  },
+};
